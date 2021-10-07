1a2d3700a1df5275a367eb888dbff60bff7cd746
trollarcurrency <arebtcfeesconstant@gmail.com>
2021-10-06 20:37:30 -0700

    tesitj

--- db/models/account.py
+++ db/models/account.py
@@ -3,7 +3,7 @@ from tortoise import fields
 
 class Account(Model):
     user = fields.OneToOneField('db.User', related_name='account', index=True)
-    address = fields.CharField(max_length=65, unique=True, index=True)
+    address = fields.CharField(max_length=66, unique=True, index=True)
 
     class Meta:
         table = 'accounts'
